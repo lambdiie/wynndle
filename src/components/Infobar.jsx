2e83fe9dec064c44ef3a70d713f8a681be96d122
DIIE123 <diiew1234@gmail.com>
2025-05-30 11:27:28 -0400

    Fix modal styling

--- src/components/Infobar.jsx
+++ src/components/Infobar.jsx
@@ -46,60 +46,63 @@ function HowToPlay() {
           beforeClose: "modal--before-close",
         }}
         overlayClassName="overlay"
+        portalClassName="portal"
         closeTimeoutMS={150}
       >
         <h1 className="container-title">How To Play</h1>
         <button onClick={closeModal}>
           <Icon path={mdiClose} size={1} color="var(--border-color)" />
         </button>
-        <p>Guess today's weapon from Wynncraft!</p>
-        <p>
-          The colour of the attributes indicates how close the guess was to the
-          correct weapon.
-        </p>
-        <p>
-          <span className="green">
-            <strong>Green</strong>
-          </span>{" "}
-          means that the attribute <em>exactly matches</em> the correct weapon.
-        </p>
-        <p>
-          <span className="yellow">
-            <strong>Yellow</strong>
-          </span>{" "}
-          means that the attribute is <em>close</em> to the correct weapon.
-        </p>
-        <ul>
-          <li>
-            For <strong>Level</strong>, your guess was within <em>5 levels</em>{" "}
-            of the correct weapon.
-          </li>
-          <li>
-            For <strong>DPS</strong>, your guess was within <em>50 DPS</em> of
-            the correct weapon.
-          </li>
-          <li>
-            For <strong>Attack Speed</strong>, your guess was <em>1 away</em>{" "}
-            from the correct weapon.{" "}
-          </li>
-          <li>
-            For <strong>Elements</strong>, your guess and the correct weapon
-            have <em>some overlap</em> in the elements.
-          </li>
-        </ul>
-        <p>
-          <span className="red">
-            <strong>Red</strong>
-          </span>{" "}
-          means that the attribute <em>does not match</em> the correct weapon.
-        </p>
-        <p>
-          <strong>Arrows</strong> (↑ and ↓) display whether the correct weapon's
-          attribute is higher or lower than your guess respectively.
-        </p>
-        <p>
-          Note: <strong>Common</strong> items have been removed from the pool.
-        </p>
+        <div className="content">
+          <p>Guess today's weapon from Wynncraft!</p>
+          <p>
+            The colour of the attributes indicates how close the guess was to the
+            correct weapon.
+          </p>
+          <p>
+            <span className="green">
+              <strong>Green</strong>
+            </span>{" "}
+            means that the attribute <em>exactly matches</em> the correct weapon.
+          </p>
+          <p>
+            <span className="yellow">
+              <strong>Yellow</strong>
+            </span>{" "}
+            means that the attribute is <em>close</em> to the correct weapon.
+          </p>
+          <ul>
+            <li>
+              For <strong>Level</strong>, your guess was within <em>5 levels</em>{" "}
+              of the correct weapon.
+            </li>
+            <li>
+              For <strong>DPS</strong>, your guess was within <em>50 DPS</em> of
+              the correct weapon.
+            </li>
+            <li>
+              For <strong>Attack Speed</strong>, your guess was <em>1 away</em>{" "}
+              from the correct weapon.{" "}
+            </li>
+            <li>
+              For <strong>Elements</strong>, your guess and the correct weapon
+              have <em>some overlap</em> in the elements.
+            </li>
+          </ul>
+          <p>
+            <span className="red">
+              <strong>Red</strong>
+            </span>{" "}
+            means that the attribute <em>does not match</em> the correct weapon.
+          </p>
+          <p>
+            <strong>Arrows</strong> (↑ and ↓) display whether the correct weapon's
+            attribute is higher or lower than your guess respectively.
+          </p>
+          <p>
+            Note: <strong>Common</strong> items have been removed from the pool.
+          </p>
+        </div>
       </Modal>
       <Tooltip id="howtoplay" />
     </div>
