76890907d1be4cb93d62567feb6bbdfbebd0cbfe
DIIE123 <diiew1234@gmail.com>
2025-06-29 14:17:42 -0400

    Set up error boundary

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import { Outlet } from "react-router-dom";
 import { useState, useEffect } from "react";
 import NavBar from "./components/NavBar/NavBar";
+import { ErrorBoundary } from "react-error-boundary";
+import Error from "./components/Main/Error";
 
 import logo from "./assets/wynndlelogo.png";
 import "./App.css";
 
 function App() {
-  const { dataArray, error, loading, errorStatus } = useFetchDatabase();
+  const { dataArray, error, loading } = useFetchDatabase();
 
   if (loading) return <p>Loading...</p>;
   if (error)
     return (
       <p>
-        Oops! Something went wrong, please refresh or try again later. Error
-        Code: {errorStatus}
+        Oops! Something went wrong with fetching the data! Try refreshing the page, or try again later. If it still doesnt work, contact @diie123 on discord with the error!
       </p>
     );
 
@@ -21,7 +22,9 @@ function App() {
     <>
       <img className="logo" src={logo} width="512" alt="Wynndle" />
       <NavBar />
-      <Outlet context={dataArray} />
+      <ErrorBoundary FallbackComponent={Error}>
+        <Outlet context={dataArray} />
+      </ErrorBoundary>
     </>
   );
 }
@@ -30,7 +33,6 @@ function useFetchDatabase() {
   const [dataArray, setDataArray] = useState(null);
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(true);
-  const [errorStatus, setErrorStatus] = useState(null);
 
   useEffect(() => {
     async function fetchDatabase() {
@@ -41,7 +43,7 @@ function useFetchDatabase() {
             Accept: "application/json",
           },
         });
-        setErrorStatus(response.status);
+
         if (response.status >= 400) {
           throw new Error("Error with fetching data!");
         }
@@ -60,7 +62,7 @@ function useFetchDatabase() {
     fetchDatabase();
   }, []);
 
-  return { dataArray, error, loading, errorStatus };
+  return { dataArray, error, loading };
 }
 
 export default App;
--- src/components/Main/Error.jsx
+++ src/components/Main/Error.jsx
@@ -1,5 +1,15 @@
-function Error() {
-  return <p>Oops! Something went wrong!</p>;
+import "./Error.css";
+
+function Error({ error }) {
+    console.log(error)
+
+  return (
+    <div role="alert">
+      <p>Oops! Something went wrong: </p>
+      <pre>{error.stack}</pre>
+      <p>If you see this page, I screwed something up. Contact @diie123 on discord with the error and I'll try to fix it!</p>
+    </div>
+  );
 }
 
 export default Error;
